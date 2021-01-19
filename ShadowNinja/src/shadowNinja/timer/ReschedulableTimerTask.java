7e3fc208d229ccb9332ad5e3005fd706469d3943
Watson <53088807+phlzy@users.noreply.github.com>
2021-01-19 12:07:30 +0800

    Add files via upload

---
+++ ShadowNinja/src/shadowNinja/timer/ReschedulableTimerTask.java
@@ -0,0 +1,17 @@
+package shadowNinja.timer;
+
+import java.util.TimerTask;
+
+public class ReschedulableTimerTask extends TimerTask {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+	
+	
+
+}
